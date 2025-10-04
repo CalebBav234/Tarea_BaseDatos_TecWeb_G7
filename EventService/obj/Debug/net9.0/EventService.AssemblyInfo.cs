b55811b2aa563b9f72af1ee75add4a567ab6d3f4
CalebBav234 <calebmbaving@gmail.com>
2025-10-04 12:43:11 -0400

    feat: CreateEventDto.cs has been created

--- EventService/obj/Debug/net9.0/EventService.AssemblyInfo.cs
+++ EventService/obj/Debug/net9.0/EventService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb4c2536b8d662c0092ae75fcfa099763e533108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d627edd554ce9ec5f07d970626de351f9ca72445")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

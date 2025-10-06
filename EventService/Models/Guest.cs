a94f707ea2e8a4137179477d2d9a198d0b53359f
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-10-06 00:35:01 -0400

    feat: Guest.cs has been created.

---
+++ EventService/Models/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventService.Models
+{
+    public class Guest
+    {
+        public Guid id { get; set; }
+
+        [Required, StringLength(100)]
+
+        public string Fullname { get; set; } = string.Empty;
+
+        public bool Confirmed { get; set; }
+
+    }
+}
